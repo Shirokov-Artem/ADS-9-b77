cb9a60eadb1157a5362b248566d90f09ea528826
Артем Широков <125153836+Shirokov-Artem@users.noreply.github.com>
2023-05-17 22:36:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,8 +3,34 @@
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
+#include  <string>
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+    std::ifstream file(filename);
+    if (!file) {
+        std::cerr << "Error opening file " << filename << std::endl;
+        exit(1);
+    }
+    std::string word;
+    BST<std::string> tree;
+    while ((word = getNextWord(file)) != "") {
+        tree.insert(word);
+    }
+    return tree;
+}
+
+std::string getNextWord(std::ifstream& file) {
+    std::string word;
+    char ch;
+    while (file.get(ch) && !isalpha(ch)) {
+    }
+    if (!file) {
+        return "";
+    }
+    word += tolower(ch);
+    while (file.get(ch) && isalpha(ch)) {
+        word += tolower(ch);
+    }
+    return word;
 }
