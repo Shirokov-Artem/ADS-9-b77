2325f2f1e45d75512b2bdfb68a76957e8406166a
Артем Широков <125153836+Shirokov-Artem@users.noreply.github.com>
2023-05-19 18:11:15 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -2,62 +2,77 @@
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 
-#include <string>
-#include <algorithm>
+#include <cstdlib>
+#include <iostream>
 
 template <typename T>
 class BST {
- private:
-  struct Node {
-    T value;
-    int count;
-    Node* left;
-    Node* right;
-    explicit Node(const T& new_value) : value(new_value), count(1), left(nullptr), right(nullptr) {}
-  };
-  Node* root;
-  Node* insert(Node* n, const T& value) {
-    if (!n) {
-      return new Node(value);
-    }
-    if (value < n->value) {
-      n->left = insert(n->left, value);
-    } else if (value > n->value) {
-      n->right = insert(n->right, value);
-    } else {
-      n->count++;
-    }
-    return n;
-  }
-  int search(Node* n, const T& value) const {
-    if (!n) {
-      return 0;
-    }
-    if (n->value == value) {
-      return n->count;
-    }
-    if (value < n->value) {
-      return search(n->left, value);
-    }
-    return search(n->right, value);
-  }
-  int depth(Node* n) const {
-    if (!n) {
-      return 0;
-    }
-    return std::max(depth(n->left), depth(n->right)) + 1;
-  }
+    struct Node {
+        T data;
+        Node* left;
+        Node* right;
+
+        Node(const T& value) : data(value), left(nullptr), right(nullptr) {}
+    };
+
+    Node* _root;
 
  public:
-  BST() : root(nullptr) {}
-  void insert(const T& value) {
-    root = insert(root, value);
-  }
-  int search(const T& value) const {
-    return search(root, value);
-  }
-  int depth() const {
-    return depth(root);
-  }
+    BST() : _root(nullptr) {}
+    virtual ~BST() {
+        destroy(_root);
+    }
+    void insert(const T& value) {
+        _root = insert(value, _root);
+    }
+    int depth() const {
+        return depth(_root);
+    }
+    int search(const T& value) const {
+        return search(value, _root);
+    }
+
+ private:
+    // рекурсивная функция для уничтожения дерева
+    void destroy(Node* p) {
+        if (p != nullptr) {
+            destroy(p->left);
+            destroy(p->right);
+            delete p;
+        }
+    }
+    // рекурсивная функция для вставки элемента в дерево
+    Node* insert(const T& value, Node* p) {
+        if (p == nullptr) {
+            return new Node(value);
+        }
+        if (value < p->data) {
+            p->left = insert(value, p->left);
+        } else if (value > p->data) {
+            p->right = insert(value, p->right);
+        }
+        return p;
+    }
+    // рекурсивная функция для подсчёта глубины дерева
+    int depth(Node* p) const {
+        if (p == nullptr) {
+            return 0;
+        }
+        return 1 + std::max(depth(p->left), depth(p->right));
+    }
+    // рекурсивная функция для поиска заданного элемента в дереве
+    int search(const T& value, Node* p) const {
+        if (p == nullptr) { // если элемент не найден
+            return -1;
+        }
+        if (value == p->data) { // если элемент найден
+            return depth(p);
+        }
+        if (value < p->data) { // если значение меньше текущего элемента, идём влево
+            return search(value, p->left);
+        }
+        // иначе идём вправо
+        return search(value, p->right);
+    }
 };
 #endif  // INCLUDE_BST_H_
