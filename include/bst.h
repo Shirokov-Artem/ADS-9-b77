c4d0153b15e2febf88a20f17d66333c3b8a85f07
Артем Широков <125153836+Shirokov-Artem@users.noreply.github.com>
2023-05-19 19:23:43 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -4,7 +4,6 @@
 
 template<typename T>
 class BST {
-
  private:
   struct Node {
     T value;
@@ -12,56 +11,42 @@ class BST {
     Node* left;
     Node* right;
   };
-  Node* root;
-  Node* addNode(Node* p, const T& value) {
-    if (p == nullptr) {
-      p = new Node{value, 1, nullptr, nullptr};
-    }
-    else if (value < p->value) {
-      p->left = addNode(p->left, value);
-    }
-    else if (value > p->value) {
-      p->right = addNode(p->right, value);
-    }
-    else {
-      p->count++;
-    }
-    return p;
-  }
-  int searchNode(Node* p, const T& value) {
-    if (p == nullptr) {
-      return 0;
-    }
-    else if (value == p->value) {
-      return p->count;
-    }
-    else if (value < p->value) {
-      return searchNode(p->left, value);
-    }
-    else {
-      return searchNode(p->right, value);
-    }
-  }
-  int getDepth(Node* p) {
-    if (p == nullptr) {
-      return 0;
-    }
-    else {
-      int leftDepth = getDepth(p->left);
-      int rightDepth = getDepth(p->right);
-      return (leftDepth > rightDepth ? leftDepth : rightDepth) + 1;
-    }
-  }
+  Node* root = nullptr;
 
  public:
-  BST() : root(nullptr) {}
-  void Add(const T& value) {
-    root = addNode(root, value);
+  BST() = default;
+  void add(const T& value) {
+    Node** cur = &root;
+    while (*cur != nullptr) {
+      if ((*cur)->value < value) {
+        cur = &(*cur)->left;
+      } else if ((*cur)->value > value) {
+        cur = &(*cur)->right;
+      } else {
+        (*cur)->count++;
+        return;
+      }
+    }
+    *cur = new Node{value, 1, nullptr, nullptr};
   }
   int search(const T& value) {
-    return searchNode(root, value);
+    Node* cur = root;
+    while (cur != nullptr) {
+      if (cur->value == value) {
+        return cur->count;
+      } else if (cur->value < value) {
+        cur = cur->left;
+      } else {
+        cur = cur->right;
+      }
+    }
+    return 0;
   }
   int depth() {
+    std::function<int(Node*)> getDepth = [&](Node* node) -> int {
+      if (!node) return 0;
+      return std::max(getDepth(node->left), getDepth(node->right)) + 1;
+    };
     return getDepth(root) - 1;
   }
 };
