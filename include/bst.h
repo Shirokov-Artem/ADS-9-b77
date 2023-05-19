c55a6bc594e5e4fbe927aebce25ca667d9a13b66
Артем Широков <125153836+Shirokov-Artem@users.noreply.github.com>
2023-05-19 19:46:03 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -2,55 +2,65 @@
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 
-#include <algorithm>
-#include <functional>
+#include <string>
+#include <iostream>
+#include <fstream>
 
-template<typename T>
+template<class T>
 class BST {
  private:
-  struct Node {
-    T value;
-    int count;
-    Node* left;
-    Node* right;
-  };
-  Node* root = nullptr;
+    struct Node {
+        T key;
+        int count;
+        Node* left;
+        Node* right;
+        Node(T k) : key(k), count(1), left(nullptr), right(nullptr) {}
+    };
+    Node* root = nullptr;
+    Node* insert(Node* cur, T key) {
+        if (!cur) {
+            return new Node(key);
+        }
+        if (cur->key == key) {
+            cur->count++;
+        } else if (cur->key > key) {
+            cur->left = insert(cur->left, key);
+        } else {
+            cur->right = insert(cur->right, key);
+        }
+        return cur;
+    }
+    int search(Node* cur, T key) {
+        if (!cur) {
+            return 0;
+        }
+        if (cur->key == key) {
+            return cur->count;
+        } else if (cur->key > key) {
+            return search(cur->left, key);
+        } else {
+            return search(cur->right, key);
+        }
+    }
+    int depth(Node* cur) {
+        if (!cur) {
+            return -1;
+        }
+        int leftDepth = depth(cur->left);
+        int rightDepth = depth(cur->right);
+        return std::max(leftDepth, rightDepth) + 1;
+    }
 
  public:
-  BST() = default;
-  void add(const T& value) {
-    Node** cur = &root;
-    while (*cur != nullptr) {
-      if ((*cur)->value < value) {
-        cur = &(*cur)->right;
-      } else if ((*cur)->value > value) {
-        cur = &(*cur)->left;
-      } else {
-        (*cur)->count++;
-        return;
-      }
+    BST() = default;
+    void add(const T& key) {
+        root = insert(root, key);
     }
-    *cur = new Node{value, 1, nullptr, nullptr};
-  }
-  int search(const T& value) {
-    Node* cur = root;
-    while (cur != nullptr) {
-      if (cur->value == value) {
-        return cur->count;
-      } else if (cur->value < value) {
-        cur = cur->right;
-      } else {
-        cur = cur->left;
-      }
+    int search(const T& key) {
+        return search(root, key);
     }
-    return 0;
-  }
-  int depth() {
-    std::function<int(Node*)> getDepth = [&](Node* node) -> int {
-      if (!node) return 0;
-      return std::max(getDepth(node->left), getDepth(node->right)) + 1;
-    };
-    return getDepth(root) - 1;
-  }
+    int calcDepth() {
+        return depth(root);
+    }
 };
 #endif  // INCLUDE_BST_H_
